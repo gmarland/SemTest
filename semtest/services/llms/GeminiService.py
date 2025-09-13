bf550f202de8a3c540becd405c1cdcd59b924a7c
Gareth Marland <gareth.marland@gmail.com>
2025-09-13 17:29:52 +0100

    working on gemini

--- semtest/services/llms/GeminiService.py
+++ semtest/services/llms/GeminiService.py
@@ -7,8 +7,8 @@ class GeminiService(LLMService):
         genai.configure(api_key=api_key)
 
     def query(self, model: str, text: str) -> str:
-        model = genai.GenerativeModel(model)
+        gemini_model = genai.GenerativeModel(model)
 
-        response = model.generate_content(text)
+        response = gemini_model.generate_content(text)
 
         return response.text
